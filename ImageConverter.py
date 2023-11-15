1083fdec8702b870025fd0e1e2744b06526980d7
Quezetl <bradleyberning@yahoo.com>
2023-11-14 20:54:00 -0800

    [Documentation]
    Added comments to code and deleted superfluous line prompting usage.

--- ImageConverter.py
+++ ImageConverter.py
@@ -4,11 +4,10 @@ from pillow_heif import register_heif_opener
 
 register_heif_opener()
 
-if input("would you like to convert photos: ").lower() != "yes":
-    exit()
-
+# Check if user wants new directory
 prompt = input("would you like to create a new directory for the images?\n1) Yes\n2) No\nAnswer: ")
 
+# Either create new directory or don't based on previous input
 if prompt == str(1) or prompt.lower() == "yes" or prompt.lower() == "y":
     newFolder = input("Enter new folder name: ")
     os.mkdir(newFolder)
@@ -16,6 +15,7 @@ if prompt == str(1) or prompt.lower() == "yes" or prompt.lower() == "y":
 else:
     newFolder = ""
 
+# Loop for checking conversion type, if invalid input it will prompt again for valid input
 while (True):
     type = int(input("What conversion would you like to make?\n"
                      "1).HEIC ->  .JPG\n"
@@ -34,6 +34,7 @@ while (True):
         case _:
             continue
 
+# Stores new and old extension type to be used later
 match type:
     case 1:
         oExt = ".HEIC"
@@ -54,13 +55,17 @@ match type:
     case _:
         print("Something went wrong\n")
 
+# main loop for converting from old file type to new file type
 for filename in os.listdir("."):
-    if filename.endswith(oExt):
-        prefix = filename.split(oExt)[0]
-        name = prefix + nExt
-        newName = newFolder + name
-        im = Image.open(filename)
-        exifInfo = im.info['exif']
-        img = im.convert("RGB")
-        img.save(name, formatType, exif=exifInfo)
-        os.renames(name, newName)
+    try:
+        if filename.endswith(oExt):
+            prefix = filename.split(oExt)[0]
+            name = prefix + nExt
+            newName = newFolder + name
+            im = Image.open(filename)
+            exifInfo = im.info['exif']
+            img = im.convert("RGB")
+            img.save(name, formatType, exif=exifInfo)
+            os.renames(name, newName)
+    except Exception as e:
+        print(f"Error converting {filename}: {e}")
